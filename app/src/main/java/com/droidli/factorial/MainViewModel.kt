412c21b85ebcf5c2f6979069542c99e2ad0ae3d2
droid li <e444er@gmail.com>
2022-04-24 16:36:59 +0600

    coroutineScope

--- app/src/main/java/com/droidli/factorial/MainViewModel.kt
+++ app/src/main/java/com/droidli/factorial/MainViewModel.kt
@@ -3,14 +3,13 @@ package com.droidli.factorial
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.math.BigInteger
 
 class MainViewModel : ViewModel() {
+
+    private val coroutineScope =
+        CoroutineScope(Dispatchers.Default + CoroutineName("My coroutine scope"))
 
     private val _state = MutableLiveData<State>()
     val state: LiveData<State> = _state
@@ -21,9 +20,9 @@ class MainViewModel : ViewModel() {
             _state.value = Error
             return
         }
-        viewModelScope.launch {
+        coroutineScope.launch(Dispatchers.Main) {
             val number = value.toLong()
-            val result = withContext(Dispatchers.Default){
+            val result = withContext(Dispatchers.Default) {
                 factorial(number)
             }
             _state.value = Factorial(result)
@@ -31,10 +30,15 @@ class MainViewModel : ViewModel() {
     }
 
     private fun factorial(number: Long): String {
-            var result = BigInteger.ONE
-            for (i in 1..number) {
-                result = result.multiply(BigInteger.valueOf(i))
-            }
-            return result.toString()
+        var result = BigInteger.ONE
+        for (i in 1..number) {
+            result = result.multiply(BigInteger.valueOf(i))
+        }
+        return result.toString()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
     }
 }
